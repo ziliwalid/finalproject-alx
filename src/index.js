de4e1603f4c4306726215c6a400145f54a5f9e6a
ziliwalid <walidzili19@gmail.com>
2024-07-16 13:39:18 +0100

    WIP / dev Components and creating cotext service to consume api with

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Home from './NormalPages/HomePage/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import these
+import { AppProvider } from './context'; // Ensure this import is correct
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
